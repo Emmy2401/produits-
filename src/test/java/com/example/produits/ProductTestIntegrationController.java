b59d869d0a6d2e04acfb462c918cfcf01a5070f1
Emmy Lemonnier <emmy.lemonnier@gmail.com>
2024-12-24 15:06:37 +0100

    test intégration avec auth : full ok

--- src/test/java/com/example/produits/ProductTestIntegrationController.java
+++ src/test/java/com/example/produits/ProductTestIntegrationController.java
@@ -9,9 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -20,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
+@WithMockUser(username = "testUser", roles = {"USER"})
 public class ProductTestIntegrationController {
     @Autowired
     private MockMvc mockMvc;
